7a498beb3b4f60533bdc928675eeef42e55b7974
Wojciech <wojciechminior@gmail.com>
2026-03-12 23:56:55 +0100

    First easy exercise added

--- challanges/Ex1.c
+++ challanges/Ex1.c
@@ -2,22 +2,30 @@
 #include <stdint.h>
 #include <assert.h>
 
-//First challange is easy. Add 1 to every element of array "byteArr". When you suceed HELLO WORLD will be displayed.
 
 
-uint8_t byteArr[] = { 0xAA,0xBB,0xCC,0xFF };
+//Write a function that, for each byte in the given array,
+//first inverts every second bit (i.e., flips bits at positions 1, 3, 5, 7), and then inverts the entire byte. 
+//The function should modify the array in place. When you succeed you will be congratulated.		
+
+
+
+uint8_t byteArr[] = { 0xAA,0xBB,0xCC,0xDD,0xFF };
 
 void game(uint8_t* array, size_t length) {
-	}
+	
+   //Place to write your code 
 }
+
+
 
 
 int main()
 {
 	game(byteArr,sizeof(byteArr));
 	int sumSolution = byteArr[0]+byteArr[1]+byteArr[2]+byteArr[3];
-	assert(564 == sumSolution);
-	printf("\tHELLO WORLD !\t\n");
+	assert(952 == sumSolution);
+	printf("\tCONGRATULATIONS!\t\n");
 	return 0;
 }
 
